757ec0cae484c0f8c62927a4b7b8e41c835606a4
wx5162839 <292476141@qq.com>
2021-06-23 12:45:58 -0700

    Install django rest

--- twitter/settings.py
+++ twitter/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
+    # third party
+    'rest_framework',
+
+    # project apps
+    'accounts',
 ]
 
 REST_FRAMEWORK = {
